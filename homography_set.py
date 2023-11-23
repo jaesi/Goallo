38140b61569a2af598bdd5c3e68876e07ba0fd3f
steamedbean <148408146+steamedbean@users.noreply.github.com>
2023-11-23 17:29:12 +0900

    Pedestrian mapping set
    ## Description ##
    
    These files include the 3 scripts, and a .zip of the folders you need to run pedestrian detection.
    
    Scripts:
    1. video_to_images.py       (imports a video, and exports individual images)
    2. homography_set.py       (sets the homography transformation matrix)
    3. detect_and_map.py       (detects pedestrians in all images, and creates a heatmap of their transformed points)
    
    Folders:
    1. detections_data       (a .csv file of the detections from the 'detect_and_map.py' will be exported to here)
    2. input_images           (the source image ('video_image') and destination image ('satellite_image') should be put here for the 'homography_set.py' script to use)
    3. input_video             (the source video should be put here for the 'video_to_images.py' script to use)
    4. output_images        (the homography point images (from the 'homography_set.py' script), and heat maps (from 'detect_and_map.py' script) will be exported to here)
    5. video_frames           (the individual frames extracted from the video by the 'video_to_images.py' script will be exported here)
    6. Yolo-Weights          (the Yolo-Weights files that are downloaded when you install )
    
    ## Instructions ##
    
    1. Create a folder for your project (you can call it anything, e.g. "pedestrian_mapping")
    2. Unzip the pedestrian_mapping_folders.zip and put all those individual folders in your project folder.
    3. Put the three scripts inside the project folder.
    4. Put a frame from your source video into the 'input_images' folder and rename to 'video_image.jpg'
    5. Put a satellite image of your site into the  'input_images' folder and rename to 'satellite_image.jpg'
    6. Add your source video to the input_video folder.
    7 Add the yolo weight files:
       a) If you have the Yolo-Weights (e.g. 'yolov8l.pt') already, put the files inside the 'Yolo-Weights' folder.
       b) If you don't have them already they will be downloaded and placed in the 'Yolo-Weights' folder automatically, when you run the 'detect_and_map.py' script.
    8. Run the scripts in the order that they are listed at the top of this document.
    
    ## Code problem advice ##
    
    If you have any problems, try scream at your computer.
    If it doesn't listen, ask God why they are testing you, then scream again.
    If God doesn't listen, try telling your computer that they're handsome.
    If that doesn't work, maybe try a cool dance.
    Maybe an even cooler dance? o.O
    
    If none of those steps work, then, I'm afraid you are down to the last and worst option.. try ask Tom.

---
+++ homography_set.py
@@ -0,0 +1,103 @@
+## CODE DESCRIPTION ##
+
+# THIS SCRIPT IS THE SECOND SCRIPT TO BE RUN IN THE 'pedestrian_mapping' SET.
+# FIRST YOU NEED TO RUN:
+# 1. 'video_to_images.py'
+
+# AFTER THIS SCRIPT YOU CAN RUN:
+# 3. 'detect_and_map.py'
+
+
+# THIS SCRIPT CREATES A HOMOGRAPHY MATRIX TO TRANSFORM ONE IMAGE TO ANOTHER.
+# IT'S PURPOSE IS TRANSFORM POINTS OF DETECTIONS OF PEOPLE FROM A CAMERA (APPROX 15DEG ANGLE) TO A TOP VIEW SATELLITE
+# IMAGE.
+# THESE POINTS ARE THEN PLOTTED TOGETHER TO CREATE A HEATMAP OF PEDESTRIAN MOVEMENT THROUGH A SPACE.
+
+
+
+## STEPS
+# 1. CREATE FUNCTION TO MANUALLY SELECT POINTS IN IMAGE.
+# 2. READ IN SOURCE IMAGE (FROM VIDEO USED TO TRACK PEDESTRIANS) AND CHOOSE 4 POINTS.
+# 3. READ IN THE DESTINATION IMAGE (TOP VIEW SATELLITE IMAGE) AND CHOOSE 4 CORRESPONDING POINTS THAT MATCH THE LOCATIONS
+#    IN THE SOURCE IMAGE.
+# 4. CONVERT EACH SET OF THE COORDINATES OF THESE POINTS TO A USABLE ARRAY.
+# 5. CREATE HOMOGRAPHY MATRIX FROM THESE TWO ARRAYS.
+
+## PREPARATION
+
+# STEP 0.1. LOAD REQUIRED PACKAGES
+import numpy as np
+from ultralytics import YOLO
+import cv2
+
+# 1. CREATE FUNCTION TO MANUALLY SELECT POINTS IN IMAGE.
+def select_point(event, x, y, flags, param):
+    global points, img_name
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        cv2.circle(param, (x, y), 5, (0, 0, 255), -1)
+        cv2.imshow(img_name, param)
+        points.append((x, y))
+
+        if len(points) == 4:
+            cv2.destroyAllWindows()
+
+# 2. READ IN SOURCE IMAGE (IMAGE FROM VIDEO USED TO TRACK PEDESTRIANS)
+img1 = cv2.imread('input_images/video_image.jpg')
+img_src = img1
+img_src = cv2.resize(img1, (600, 800))
+
+# 3. READ IN DESTINATION IMAGE (TOP VIEW SATELLITE IMAGE YOU WANT TO CONVERT TO)
+img2 = cv2.imread('input_images/satellite_image.jpg')
+img_dst = img2
+img_dst = cv2.resize(img2, (600,800))
+
+# 이미지 1에서 꼭지점 선택
+points = []
+img_name = 'Image 1'
+cv2.imshow(img_name, img_src)
+cv2.setMouseCallback(img_name, select_point, img_src)
+cv2.waitKey(0)
+
+points_img1 = points.copy()
+
+# 이미지 2에서 꼭지점 선택
+points = []
+img_name = 'Image 2'
+cv2.imshow(img_name, img_dst)
+cv2.setMouseCallback(img_name, select_point, img_dst)
+cv2.waitKey(0)
+
+points_img2 = points.copy()
+
+# print("Selected points in Image 1:", points_img1)
+# print("Selected points in Image 2:", points_img2)
+
+# 4. CONVERT EACH SET OF THE COORDINATES OF THESE POINTS TO A USABLE ARRAY.
+point_src = np.array(points_img1).reshape(4,2)
+point_dst = np.array(points_img2).reshape(4,2)
+
+print(point_src)
+print(point_dst)
+
+# 5. CREATE HOMOGRAPHY MATRIX FROM THESE TWO ARRAYS.
+# 호모그라피에서 변환된 행렬 찾기
+# h가 변환에 이용된 바로, 3 X 3 의 행렬이다.
+
+h, status = cv2.findHomography(point_src, point_dst)
+# 호모그라피 행렬 호출용
+def get_homography_matrix():
+    return h
+
+img_output = cv2.warpPerspective(img_src, h, (img_src.shape[1], img_src.shape[0]))
+
+cv2.imshow('SRC', img_src)
+cv2.imshow('DST', img_dst)
+cv2.imshow('Warp', img_output)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+cv2.imwrite('output_images/source_image_hom_points.jpg', img_src)
+cv2.imwrite('output_images/destination_image_hom_points.jpg', img_dst)
+cv2.imwrite('output_images/transformation_image_hom_points.jpg', img_output)
---
+++ video_to_images.py
@@ -0,0 +1,59 @@
+## CODE DESCRIPTION ##
+
+# THIS SCRIPT IS THE FIRST SCRIPT TO BE RUN IN THE 'pedestrian_mapping' SET.
+# AFTERWARD YOU CAN RUN:
+# 2. 'homography_set.py'
+# 3. 'detect_and_map.py'
+
+# THIS SCRIPT EXTRACTS AND SAVES FRAMES FROM A VIDEO
+# IT'S PURPOSE IS TO CREATE A SET OF IMAGES FOR THE FOLLOWING SCRIPTS TO USE:
+# 1. 'homography_set.py'
+# 2. 'detect_and_map.py'
+
+
+
+## STEPS
+# 1. IMPORT VIDEO
+# 2. ASSIGN FRAME INTERVAL
+# 3. EXPORT IMAGES
+
+
+import cv2
+import os
+
+def export_frames(video_path, output_folder, interval_sec=1):
+    # Open the video file
+    cap = cv2.VideoCapture(video_path)
+
+    # Create the output folder if it doesn't exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Get frames per second and calculate frames to skip for the desired interval
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    frame_skip = int(fps * interval_sec)
+
+    # Read and export frames
+    frame_count = 0
+    while True:
+        ret, frame = cap.read()
+
+        if not ret:
+            break
+
+        # Export frame every 'frame_skip' frames
+        if frame_count % frame_skip == 0:
+            frame_filename = f"frame_{int(frame_count / frame_skip):04d}.png"
+            frame_path = os.path.join(output_folder, frame_filename)
+            cv2.imwrite(frame_path, frame)
+
+        frame_count += 1
+
+    # Release the video capture object
+    cap.release()
+
+if __name__ == "__main__":
+    video_path = "input_video/Seoul Plaza mist.mp4"  # Replace with your video file path
+    output_folder = "video_frames"  # Replace with your desired output folder
+
+    export_frames(video_path, output_folder, interval_sec=1) # Set the frame rate that you want to extract
